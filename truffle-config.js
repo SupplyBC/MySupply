972930f7f074bd90fb34027a0506f4341dafd8fb
SupplyBC <mohamedkaramshehata@gmail.com>
2021-04-02 19:30:59 +0200

    Modified truffle-config.json

--- truffle-config.js
+++ truffle-config.js
@@ -6,21 +6,20 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      host: '127.0.0.1',
-      network_id: '*',
-      port: 7545
+      host: "127.0.0.1",
+      network_id: "*",
+      port: 7545,
     },
 
     mobile_develop: {
-
-      host: '192.168.1.7',
-      network_id: '*',
-      port: 7545
-    }
+      host: "192.168.1.11",
+      network_id: "*",
+      port: 7545,
+    },
   },
   compilers: {
     solc: {
-      version: '0.7.4',
+      version: "0.7.4",
       settings: {
         optimizer: {
           enabled: true,
