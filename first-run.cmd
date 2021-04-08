e3ebf07b47b1769db5786ea0fd4c6df16d61a7de
SupplyBC <mohamedkaramshehata@gmail.com>
2021-04-08 18:35:26 +0200

    Modified script

--- first-run.cmd
+++ first-run.cmd
@@ -6,14 +6,14 @@ ECHO 1- SETTING UP FILES AND DEPENDENCIES
 ECHO ---------------------------
 cd client
 CALL npm i --save
+ECHO.
 ECHO DEPENDENCIES SET SUCCESSFULLY.
-ECHO. 
 ECHO 2- SETTING UP SMART CONTRACTS
 ECHO ---------------------------
 cd .. 
 CALL truffle migrate --reset --network mobile_develop
 ECHO SMART CONTRACTS SET SUCCESSFULLY.
-ECHO.
 ECHO PROJECT IS ALL SET, TO RUN YOUR APPLICATION EXECUTE RUN.CMD
+ECHO.
 cmd /k
 
